a9912d32034b44b4c35abedd2a348e2101b10688
Rob Bearman <robbear@hyperfine.com>
2014-12-30 10:14:20 -0800

    Introduce PackageItem and begin work on PackageAnalyzer routines with unit tests.

--- test/server/packageAnalyzer.js
+++ test/server/packageAnalyzer.js
@@ -7,12 +7,14 @@ var path = require( "path" );
 var should = require( "chai" ).should();
 var PackageAnalyzer = require( "../../lib/packageAnalyzer" );
 var packageAnalyzer;
+var rootDirectory;
 
 describe( "packageAnalyzer", function() {
 
   before( function( done ) {
     packageAnalyzer = new PackageAnalyzer();
     packageAnalyzer.setLoggerLevel( "error" );
+    rootDirectory = process.cwd();
     done();
   });
 
@@ -24,12 +26,47 @@ describe( "packageAnalyzer", function() {
     done();
   });
 
+  /*
   it( "should have the current directory set to test/server/cases/test1", function( done ) {
-    process.chdir( "./test/server/cases/test1" );
-    var currentDirectory = packageAnalyzer.getCurrentDirectory();
+    process.chdir( rootDirectory + "/test/server/cases/test1" );
+    var currentDirectory = process.cwd();
     should.exist( currentDirectory );
     path.basename( currentDirectory).should.equal( "test1" );
     done();
   });
+  */
 
+  it( "should read the package.json file in cases/test1", function( done ) {
+    packageAnalyzer.loadPackageJson( rootDirectory + "/test/server/cases/test1" )
+    .then( function( json ) {
+      should.exist( json );
+      should.exist( json.name );
+      should.exist( json.dependencies );
+      json.name.should.equal( "testproj" );
+    })
+    .done( function() {
+      done();
+    });
+  });
+
+  it ( "should handle a non-existent package.json file ", function( done ) {
+    packageAnalyzer.loadPackageJson( rootDirectory + "/bogus/path/to/nowhere" )
+    .then( function( json ) {
+      should.not.exist( json );
+    })
+    .done( function() {
+      done();
+    });
+  });
+
+  it( "should analyze the /cases/test1/node_modules path", function( done ) {
+    packageAnalyzer.analyzeNodeModulesPath( rootDirectory + "/test/server/cases/test1/node_modules" )
+    .then( function() {
+      should.exist( packageAnalyzer.packages );
+      packageAnalyzer.numPackages().should.equal( 1 );
+    })
+    .done( function() {
+      done();
+    });
+  });
 });
