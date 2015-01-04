89eacf8549219754b392011585bd2b15045d1d71
Rob Bearman <robbear@hyperfine.com>
2015-01-04 15:08:17 -0800

    Remove test/preptests.sh bash script and replace with prepare-tests Grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 var Promise = require( "bluebird" );
 var path = require( "path" );
-var fs = Promise.promisifyAll( require( "fs" ) );
+var fs = Promise.promisifyAll( require( "fs-extra" ) );
+var promiseBatcher = require( "./lib/promise-batcher" );
 
 module.exports = function( grunt ) {
 
@@ -40,6 +41,27 @@ module.exports = function( grunt ) {
 
   grunt.registerTask( "test", [ "simplemocha" ]);
 
+  grunt.registerTask( "prepare-tests", "Prepare the unit tests by running npm install", function() {
+    var casesRoot = "./test/server/cases/";
+    var done = this.async();
+    fs.readdirAsync( "./test/server/cases/" )
+    .then( function( files ) {
+      var testDirs = [];
+      for ( var i = 0; i < files.length; i++ ) {
+        var path = casesRoot + files[ i ];
+        if ( fs.lstatSync( path ).isDirectory() ) {
+          testDirs.push( path );
+        }
+      }
+
+      return testDirs;
+    })
+    .then( function( testDirs ) {
+      return promiseBatcher.batch( 1, testDirs, null, cleanAndNpmInstall );
+    })
+    .done( done );
+  });
+
   grunt.registerTask( "ckupdate", "Execute the ckupdate tool", function() {
     var done = this.async();
     var childProcess = require( "child_process" );
@@ -50,3 +72,32 @@ module.exports = function( grunt ) {
   });
 
 };
+
+function cleanAndNpmInstall( testDir ) {
+  var cwd = process.cwd();
+  console.log( "Working directory: " + cwd );
+  console.log( "Removing directory: " + testDir + "/node_modules/" );
+  return fs.removeAsync( testDir + "/node_modules/" )
+  .then( function() {
+    var promise = new Promise( function( resolve, reject ) {
+      console.log( "Changing dir to: " + testDir );
+      process.chdir( testDir );
+      var childProcess = require( "child_process" );
+      console.log( "Running npm install" );
+      var worker = childProcess.exec( "npm install" );
+      worker.stdout.pipe( process.stdout );
+      worker.stderr.pipe( process.stderr );
+      worker.on( "close", function() {
+        console.log( "On close -- changing directory to: " + cwd );
+        process.chdir( cwd );
+        resolve();
+      });
+      worker.on( "error", function() {
+        console.log( "On error -- changing directory to: " + cwd );
+        process.chdir( cwd );
+      });
+    });
+
+    return promise;
+  });
+}
--- test/README.md
+++ test/README.md
@@ -1,2 +1,2 @@
-Test cases are listed under server/cases/test*. Note that you must run ./preptests.sh in order
+Test cases are listed under server/cases/test*. Note that you must run "grunt prepare-tests" in order
 to generate the node_modules directories for each test case prior to running "grunt test".
